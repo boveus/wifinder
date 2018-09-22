e11bf0464e218fdb0f9da3f170fded2fec01d332
brandon <brandon@my-vm.com>
2018-09-22 13:43:29 -0400

    add find_by method

--- lib/models/packet.rb
+++ lib/models/packet.rb
@@ -29,6 +29,18 @@ class Packet
     [@id, @capturetime, @source, @destination, @protocol, @info]
   end
 
+  # example arguments={source: "Microsof_bd:7f:f8"}
+  def self.find_by(arguments)
+    column = arguments.keys.first
+    value = arguments.values.first
+    db = SQLite3::Database.new("./db/wifinder.db")
+    packets = []
+    db.execute( "select * from packets WHERE #{column} = '#{value}'" ) do |row|
+      packets << Packet.create_from_row(row)
+    end
+    packets
+  end
+
   def self.all
     db = SQLite3::Database.new("./db/wifinder.db")
     packets = []

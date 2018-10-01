fec0843ee6f1a4b0371fd35f2322bd0bd38236c3
brandon <brandon@my-vm.com>
2018-10-01 19:02:55 -0400

    modify monitor task

--- tasks/set_to_monitor.rb
+++ tasks/set_to_monitor.rb
@@ -1,4 +1,4 @@
-desc 'set the specified device to monitor mode'
+desc 'Usage: interface=wlan1 rake set_to_monitor | set the device to monitor mode'
 task :set_to_monitor do
   interface = "#{ENV["interface"]}"
   unless interface == ''
@@ -7,6 +7,6 @@ task :set_to_monitor do
     `sudo ifconfig #{interface} up`
   else
     puts "ERROR: No device specified! \n"
-    puts "Usage: interface=wlan10 rake set_to_monitor"
+    puts "Usage: interface=wlan1 rake set_to_monitor"
   end
 end

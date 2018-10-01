5e7aece7ea5ae5cf951001f12168c042cb5db7cc
brandon <brandon@my-vm.com>
2018-10-01 19:01:40 -0400

    add a few rake tasks

---
+++ tasks/set_to_monitor.rb
@@ -0,0 +1,12 @@
+desc 'set the specified device to monitor mode'
+task :set_to_monitor do
+  interface = "#{ENV["interface"]}"
+  unless interface == ''
+    `sudo ifconfig #{interface} down`
+    `sudo iwconfig #{interface} mode monitor`
+    `sudo ifconfig #{interface} up`
+  else
+    puts "ERROR: No device specified! \n"
+    puts "Usage: interface=wlan10 rake set_to_monitor"
+  end
+end

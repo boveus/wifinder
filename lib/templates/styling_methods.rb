ebb2c35c8e1ccf38ba84ad51bf6b5d0a0562c747
Brandon Stewart <boveus@gmail.com>
2019-03-10 16:08:16 -0400

    Add methods for defining default buttons and header/p tag css.

--- lib/templates/styling_methods.rb
+++ lib/templates/styling_methods.rb
@@ -15,4 +15,21 @@ module StylingMethods
   def css
     "<link href='https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css' rel='stylesheet'>"
   end
+
+  def header_classes
+    "\"text-5x1 font-mono text-grey-darkest text-center pb-5\""
+  end
+
+  def p_classes
+    "\"text-5x1 font-mono text-grey-darkest\""
+  end
+
+  def button_to(href, text, classes=nil)
+    "<a href='#{href}'>
+      <button class=\"bg-white hover:bg-grey-lightest text-grey-darkest
+      font-semibold py-2 px-4 border border-grey-light rounded shadow #{classes}\">
+         #{text}
+      </button>
+    </a>"
+  end
 end
